579c758856e7350fc2accf932317df7d1885cfb0
tanu <tanu@gmail.com>
2025-07-14 15:01:04 +0530

    added even odd codes

--- Array.java
+++ Array.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -85,23 +85,97 @@ public class Array {
         // }
         // System.out.println("2nd largest element is " + max1);
 
-        for (int i = 0; i <= n - 1; i++) {
-            for (int j = 0; j <= n - i - 2; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+        // for (int i = 0; i <= n - 1; i++) {
+        //     for (int j = 0; j <= n - i - 2; j++) {
+        //         if (arr[j] > arr[j + 1]) {
+        //             int temp = arr[j];
+        //             arr[j] = arr[j + 1];
+        //             arr[j + 1] = temp;
+        //         }
+        //     }
+        // }
+        // Arrays.sort(arr);
+        // System.out.println("Sorted array is:");
+        // for (int i = 0; i <= n - 1; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+        // System.out.println();
+        // System.out.println("largest element is " + arr[n - 1]);
+        // System.out.println("2nd largest element is " + arr[n - 2]);
+        // System.out.println("smallest element is " + arr[0]);
+        // System.out.println("2nd smallest element is " + arr[1]);
+        // int fact = sc.nextInt();
+        // for(int i =0; i<=n-1;i++){
+        //     if(arr[i]%fact == 0){
+        //         System.out.println(arr[i] + " is divisible by " + fact);
+        //     } else {
+        //         System.out.println(arr[i] + " is not divisible by " + fact);
+        //     }
+        // }
+        // int sum = arr[0];
+        // for(int i=1;i<=n-1;i++){
+        //     sum+=sum;
+        //     arr[i]=sum;
+        // }
+        // for(int i=0;i<=n-1;i++){
+        //     System.out.print(arr[i] + " ");
+        // }
+        // int ev =0;
+        // int esum=0;
+        // int oc = 0;
+        // int osum = 0;
+        // for(int i =0;i<=n-1;i++){
+        //     if(arr[i]%2==0){
+        //         ev++;
+        //         esum += arr[i];
+        //     } else {
+        //         oc++;
+        //         osum += arr[i];
+        //     }
+        // }
+        // System.out.println("Number of even elements: " + ev);
+        // System.out.println("Number of odd elements: " + oc);
+        // System.out.println(esum+ " "+osum);
+        // int x = sc.nextInt();
+        // int y = sc.nextInt();
+        // int count = 0;
+        // for(int i=0;i<=n-1;i++){
+        //     if(arr[i]%x==0 && arr[i]% y==0){
+        //         count++;
+        //     }
+        // }
+        // System.out.println(count + " elements are divisible by " + x + " and " + y);
+        // for(int i =0;i<=n-1;i++){
+        //     if(arr[i]%2==0){
+        //         System.out.println(i + " is even");
+        //     } else {
+        //         System.out.println(i + " is odd");
+        //     } 
+        // }
+        // int evenIndex = 0;
+        // int oddIndex = 0;
+        // for(int i =0;i<=n-1;i++){
+        //     if(arr[i]%2==0){
+        //         //System.out.println(i + " is even");
+        //         evenIndex = i;
+        //     } else {
+        //         //System.out.println(i + " is odd");
+        //         oddIndex = i;
+        //     } 
+        // }
+        // System.out.println("Last even index: " + evenIndex);
+        // System.out.println("Last odd index: " + oddIndex);
+        int sum = 0;
+        for(int i=0;i<=n-1;i++){
+            sum +=arr[i];
+        }
+        double avg = (double) sum / n;
+        System.out.println("Average is: " + avg);
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>avg){
+                System.out.print(arr[i] + " ");
             }
         }
-        System.out.println("Sorted array is:");
-        for (int i = 0; i <= n - 1; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-        System.out.println("largest element is " + arr[n - 1]);
-        System.out.println("2nd largest element is " + arr[n - 2]);
-        System.out.println("smallest element is " + arr[0]);
-        System.out.println("2nd smallest element is " + arr[1]);
+
     }
 }
