65cd260ed6f6cc8fe443e4e733b33992ac2ad80c
tanu <tanu@gmail.com>
2025-07-15 11:37:10 +0530

    `Added input functionality to TwoDarray.java: reads row and column sizes, populates 2D array, and prints its contents.`

--- TwoDarray.java
+++ TwoDarray.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class TwoDarray {
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] arr = new int[r][c];
+        for(int i=0;i<=r-1;i++){
+            for(int j=0;j<=c-1;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<=r-1;i++){
+            for(int j=0;j<=c-1;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
     
 }
